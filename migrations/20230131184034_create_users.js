b46957d708cf90016a880936348889bd6a90902c
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2023-02-08 06:27:56 -0500

    Update 20230131184034_create_users.js

--- migrations/20230131184034_create_users.js
+++ migrations/20230131184034_create_users.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
   //create our table and cols
   return knex.schema.createTable('users', function (table) {
     table.increments('id').primary();
-    table.string('username').notNullable()
+    table.string('username').notNullable().unique();
     table.string('bio');
     table.string('password').notNullable();
   })
